ff7b57446b587b75208055a011880271705b30f1
venky <tvtest.april2020@gmail.com>
2025-06-19 17:18:19 +0530

    3rd commit

---
+++ src/ex_01_java_variable/Firstprogram.java
@@ -0,0 +1,11 @@
+package ex_01_java_variable;
+
+public class Firstprogram {
+    public static void main(String[] args){
+        int a=10;
+         a=20;
+        System.out.println(a);
+
+
+    }
+}
